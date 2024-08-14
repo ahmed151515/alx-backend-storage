bc11e9faf3a32dd746bdbad315d6692a43881894
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-14 12:32:12 +0300

    .

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,6 +16,39 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """_summary_
+
+        Args:
+            data (Union[str, bytes, int, float]): _description_
+
+        Returns:
+            str: _description_
+        """
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float]:
+
+        data = self._redis.get(key)
+        if fn is not None:
+            data = fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """_summary_
+
+        Args:
+            key (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        data = (self.get(key)).decode('utf-8')
+        return data
+
+    def get_int(self, key: str) -> int:
+
+        data = int(self.get(key))
+        return data
