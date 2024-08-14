cc04503dfa3f865507136a51a4b17d0d243ba865
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-14 12:22:47 +0300

    .

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """_summary_
     """
 
+from typing import Union
 import redis
 import uuid
 
@@ -14,7 +15,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: object) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
